230212ee4eda6a1e8e94f6b075639743b7cbe931
seth <coinlawyer20757@gmail.com>
2021-07-26 22:28:01 +0100

    Use low CPU usage mode
    Optimise performance

--- scripts/Control.gd
+++ scripts/Control.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	OS.low_processor_usage_mode
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
